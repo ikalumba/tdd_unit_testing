ff060f1a795faae07fb13a2e9225616b9ef681fb
NAKAWINIFRED <winifrednakawunde07@gmail.com>
2024-07-07 00:36:08 +0300

    run second passing test

--- test/test.js
+++ test/test.js
@@ -5,5 +5,7 @@ describe('multiply tests' , function() {
     it('tdd cycle 1' , function(){
         assert.equal(multiply(1, 1) ,1);
     });
-    
+    it('tdd cycle 2' , function (){
+        assert.equal(multiply(2, 2) ,4);
+    });
 });
