ff815143b532edd9c719d86f346b4cd3cc7ea6ed
Isaac Kalumba <ikalumba221@gmail.com>
2024-07-07 16:34:38 +0300

    run cycle 3

--- test/test.js
+++ test/test.js
@@ -1,11 +1,14 @@
-import {assert} from'chai';
-import {multiply} from'../multiply.js';
+import { assert } from "chai";
+import { multiply } from "../multiply.js";
 
-describe('multiply tests' , function() {
-    it('tdd cycle 1' , function(){
-        assert.equal(multiply(1, 1) ,1);
-    });
-    it('tdd cycle 2' , function (){
-        assert.equal(multiply(2, 2) ,4);
-    });
+describe("multiply tests", function () {
+  it("tdd cycle 1", function () {
+    assert.equal(multiply(1, 1), 1);
+  });
+  it("tdd cycle 2", function () {
+    assert.equal(multiply(2, 2), 4);
+  });
+  it("Test First TDD Cycle 3", function () {
+    assert.equal(multiply(3, 3), 9);
+  });
 });
