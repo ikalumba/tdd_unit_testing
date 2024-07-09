9898524382fed874d1d480c7c8fce619f27174c8
NAKAWINIFRED <winifrednakawunde07@gmail.com>
2024-07-09 23:30:54 +0300

    refactor multiply function

--- multiply.js
+++ multiply.js
@@ -1,3 +1,17 @@
 export function multiply(a, b) {
-   return a*b
-}
+   // return a*b;
+   
+      let result = 0;
+      let absB = Math.abs(b); // handling negative numbers by taking absolute value
+      
+      for (let i = 0; i < absB; i++) {
+          result += a;
+      }
+      
+      // handling negative result if one of the numbers is negative
+      if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
+          result = -result;
+      }
+      
+      return result;
+  }
